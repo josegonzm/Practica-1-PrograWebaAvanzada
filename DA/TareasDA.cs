c3d8a9b3486ae6d60d773e64147af9de6efe6a3b
RehnamRubio <133930517+RehnamRubio@users.noreply.github.com>
2024-02-13 21:02:34 -0600

    Creacion del DA de Tareas( en proceso)

---
+++ DA/TareasDA.cs
@@ -0,0 +1,51 @@
+﻿using Abstracciones.DA;
+using Abstracciones.Modelos;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA
+{
+public class TareasDA : ITareasDA
+    {
+
+        private IRepositorioDapper _repositorioDapper;
+        private SqlConnection _sqlConnection;
+
+        public  TareasDA(IRepositorioDapper repositorioDapper)
+        {
+            _repositorioDapper = repositorioDapper;
+            _sqlConnection = repositorioDapper.ObtenerRepositorioDapper();
+        }
+
+        public Task<Guid> ActualizarTareas(Guid id, string nombre, string descripcion, DateTime fecha_inicio, DateTime fecha_fin, Usuarios asignados, string estado)
+        {
+            string sql = "@[ActualizarTarea]"; 
+            
+                
+        }
+
+        public Task<Guid> AgregarTareas(Guid id, string nombre, string descripcion, DateTime fecha_inicio, DateTime fecha_fin, Usuarios asignados, string estado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> EliminarTareas(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Tareas>> MostrarTareas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tareas> MostrarTareasPorID(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
