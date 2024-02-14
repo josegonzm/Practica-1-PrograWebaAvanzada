8811d2f5eff06b6dd32720faa079dd38d69cf7c3
RehnamRubio <133930517+RehnamRubio@users.noreply.github.com>
2024-02-13 20:49:22 -0600

    Abstracciones Tareas Terminado

---
+++ Abstracciones/API/ITareasController.cs
@@ -0,0 +1,38 @@
+﻿using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.API
+{
+  public interface ITareasController
+    {
+
+
+        [HttpGet]
+        Task<IActionResult> GetAsync();
+
+        [HttpGet]
+        Task<IActionResult> GetAsync(Guid id);
+
+        [HttpPut]
+        Task<IActionResult> PutAsync([FromQuery] Guid id, [FromBody]
+
+        string nombre, [FromBody] string descripcion, [FromBody] DateTime fecha_inicio, [FromBody] DateTime fecha_fin , [FromBody] Usuarios asignado ,  
+       
+         [FromBody] string estado     );
+
+        [HttpDelete]
+        Task<IActionResult> DeleteAsync([FromQuery] Guid id);
+
+        [HttpPost]
+        Task<IActionResult> PostAsync([FromBody] Tareas tareas);
+
+
+
+
+    }
+}
---
+++ Abstracciones/BW/ITareasBW.cs
@@ -0,0 +1,25 @@
+﻿using Abstracciones.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.BW
+{
+    public interface ITareasBW
+    {
+        Task<IEnumerable<Tareas>> MostrarTareas();
+        Task<Abstracciones.Modelos.Tareas> MostrarTareasPorID(Guid id);
+        Task<Guid> AgregarTareas(Guid id, string nombre, string descripcion, DateTime fecha_inicio, DateTime fecha_fin, Usuarios asignados, String estado);
+        Task<Guid> ActualizarTareas(Guid id, string nombre, string descripcion, DateTime fecha_inicio, DateTime fecha_fin, Usuarios asignados, String estado);
+        Task<Guid> EliminarTareas(Guid id);
+
+
+
+
+
+
+
+    }
+}
