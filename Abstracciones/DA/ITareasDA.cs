981402bfc47352df4111b8d2ec4328739262e5f0
RehnamRubio <133930517+RehnamRubio@users.noreply.github.com>
2024-02-13 20:41:31 -0600

    Agregar abstraciones de Tareas

---
+++ Abstracciones/DA/ITareasDA.cs
@@ -0,0 +1,26 @@
+﻿using Abstracciones.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.DA
+{
+    public interface ITareasDA
+    {
+        Task<IEnumerable<Tareas>> MostrarTareas();
+        Task<Abstracciones.Modelos.Tareas> MostrarTareasPorID(Guid id);
+        Task<Guid> AgregarTareas(Guid id,string nombre, string descripcion, DateTime fecha_inicio, DateTime fecha_fin, Usuarios asignados , String estado);
+        Task<Guid> ActualizarTareas(Guid id,string nombre, string descripcion, DateTime fecha_inicio, DateTime fecha_fin, Usuarios asignados, String estado);
+        Task<Guid> EliminarTareas(Guid id);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Abstracciones/Entities/Tareas.cs
@@ -0,0 +1,41 @@
+﻿using Abstracciones.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Entities
+{
+    public class Tareas
+
+    {
+        public Guid Id  { get; set; } = Guid.NewGuid();
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public DateTime Fecha_Inicio { get; set; }
+
+        public DateTime Fecha_Fin { get; set; }
+
+        public Usuarios Asignado { get; set; }
+
+        public string Estado { get; set; } 
+
+
+
+
+
+    
+
+       
+        
+
+
+
+
+
+
+
+    }
+}
